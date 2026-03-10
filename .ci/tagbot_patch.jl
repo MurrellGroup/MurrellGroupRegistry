3b1423aa4a8a10b9bdb30494547941df98bb5a24
AntonOresten <antonoresten@proton.me>
2026-03-10 19:58:11 +0100

    Update tagbot_patch.jl

--- .ci/tagbot_patch.jl
+++ .ci/tagbot_patch.jl
@@ -12,7 +12,7 @@ end
         since = string(now(UTC) - Day(3))
         commit_list, _ = GH.commits(repo; auth=AUTH[], params=(; since, per_page=100))
 
-        repos_versions = map(commit_list) do c
+        registrations = map(commit_list) do c
             msg = c.commit.message
             # LocalRegistry format:
             #   New version: PkgName v1.2.3
@@ -20,17 +20,19 @@ end
             #   UUID: ...
             #   Repo: https://github.com/Org/PkgName.jl
             #   Tree: ...
+            m = match(r"New version:\s+(\S+)\s+v(\S+)", first(split(msg, '\n')))
+            pkg_name = m === nothing ? nothing : strip(m[1])
+            version = m === nothing ? nothing : "v$(strip(m[2]))"
             m = match(r"Repo: .*github\.com[:/](.*)", msg)
             pkg_repo = m === nothing ? nothing : strip(m[1])
             pkg_repo !== nothing && endswith(pkg_repo, ".git") && (pkg_repo = pkg_repo[1:end-4])
-            m = match(r"v(\S+)", first(split(msg, '\n')))
-            version = m === nothing ? nothing : "v$(strip(m[1]))"
-            (pkg_repo, version)
+            (pkg_repo, pkg_name, version)
         end
 
-        filter!(rv -> first(rv) !== nothing, repos_versions)
-        unique!(first, repos_versions)
-        for (pkg_repo, version) in repos_versions
+        filter!(rv -> rv[1] !== nothing && rv[2] !== nothing && rv[3] !== nothing, registrations)
+        unique!(registrations)
+        for (pkg_repo, pkg_name, version) in registrations
+            @info "Processing version $version of $pkg_repo ($pkg_name)"
             maybe_notify(event, pkg_repo, version; cron=true)
         end
     end
